291e9a5b5433dc9c3d8b0011a47a4b09a47a60b6
yagzblt <yagzblt@gmail.com>
2023-11-21 14:58:26 +0300

    second

--- flask-01-hello-world-app/hello-world-app.py
+++ flask-01-hello-world-app/hello-world-app.py
@@ -1,10 +1,17 @@
 from flask import Flask 
 app = Flask(__name__)
 
+@app.route('/')
+def head():
+    return "hello word"
 
+@app.route('/second')
+def second():
+    return 'This is second page'
 
-
-
+@app.route('/third')
+def third():
+    return 'This is third page and I have written it!'
 
 if __name__ == '__main__':
 
